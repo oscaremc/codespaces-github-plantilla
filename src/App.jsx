7e177de779d87771ea533f507ee708d62b8d4182
Oscar Eduardo Montalvo Camacho <45255751+oscaremc@users.noreply.github.com>
2024-08-09 19:41:52 +0000

    Add text personalized with oemc

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Oscar Eduardo Montalvo Camacho",
+  title: "Web Developer",
+  email: "oscar@example.com",
+  gitHub: "https://github.com/oscaremc",
+  instagram: "https://www.instagram.com/oscaremcdev/",
+  linkedIn: "https://www.linkedin.com/feed/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://x.com/home",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/fondo_mosaico.png";
 
 const imageAltText = "purple and blue abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description = 
+"SI'm Oscar Eduardo Montalvo Camacho, a frontend developer passionate about turning ideas into technological solutions, with a focus on efficiency and innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
+  "Self-taught student",
   "Web design",
   "User experience",
   "Inclusive design",
   "Focus group testing",
   "Mobile user interfaces",
-  "Graphic design",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Driven by my curiosity in software, finance, and outer space, I'm dedicated to using technology to make the world a more efficient place.";
 
 const About = () => {
   return (
